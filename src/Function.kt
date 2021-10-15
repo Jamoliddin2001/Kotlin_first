123dcabeab399c180b7246e41dc92b0fb978c51a
Jamoliddin Sharipov <sh.jamoliddin2001@gmail.com>
2021-10-15 19:03:04 +0300

    Initial commit

---
+++ src/Function.kt
@@ -0,0 +1,30 @@
+fun main(){
+//    var name="Jamoliddin"
+//    println("$name")
+    val x:Int?=null
+    println(x)
+    println((1..3).toList())
+    println('A' in 'A'..'z')
+    var a= arrayOf(1,2,3,4,5,6)
+    println("a.size="+a.size)
+    println((Math.random()*1000).toInt())
+    println(Math.pow(2.0,5.0))
+    println((1 until 5).toList())
+    println(Mymax(10))
+    println(first(1))
+    `*****`()
+}
+
+private fun Mymax(a:Int):Int{
+    var b=a;
+    b++
+    return b
+}
+
+fun `*****`() {
+    println("Jamoliddin")
+}
+
+private fun first(a:Int=2){
+    var b=a
+}
