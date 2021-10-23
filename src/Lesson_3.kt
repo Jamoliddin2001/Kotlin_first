4df404c03dfd1357551881380be01587c0f43b8b
Jamoliddin Sharipov <sh.jamoliddin2001@gmail.com>
2021-10-23 17:27:27 +0300

    update 2.6.1

--- src/Lesson_3.kt
+++ src/Lesson_3.kt
@@ -3,9 +3,6 @@ fun main(){
     student1.name="Jamoliddin"
     println(student1.name)
 
-    for(i in 1 until 100 step 10){
-        println("$i")
-    }
     var x=10;
     println(x.isPositive());
 
