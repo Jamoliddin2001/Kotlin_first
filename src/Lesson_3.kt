b8ccbb184aa08fade574089b67fff808b399d865
Jamoliddin Sharipov <sh.jamoliddin2001@gmail.com>
2021-10-23 17:24:56 +0300

    update 2.6

--- src/Lesson_3.kt
+++ src/Lesson_3.kt
@@ -2,4 +2,13 @@ fun main(){
     var student1=Student()
     student1.name="Jamoliddin"
     println(student1.name)
+
+    for(i in 1 until 100 step 10){
+        println("$i")
+    }
+    var x=10;
+    println(x.isPositive());
+
 }
+
+fun Int.isPositive()=this>=0
